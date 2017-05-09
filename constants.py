0fdde7a7402ded7e1266f58ef228d88b37ff55c0
Ravi Prasad <r58641@gmail.com>
2017-05-09 09:19:49 +0530

    added the constants

---
+++ constants.py
@@ -0,0 +1,7 @@
+F_CARRIER = 1000 # ie., 1000Hz = 1Khz carrier
+BITRATE = 50
+DEVIATIOON = 500 # ie., 500Hz = 0.5Khz
+N = 64 # Number of bits to send
+AMPLITUDE = 1
+F_SAMPLING = 10000 # 10Khz
+AMPLITUDE_NOISE = 0.1
--- input_vco.py
+++ input_vco.py
@@ -1,3 +1,4 @@
 import numpy as np
+from constants import N
 
 unmodulated_input = np.random.random_integers(0,1,N)
